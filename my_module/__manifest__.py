3ecad426ff437e89eadfaf77e5980ef91df0e219
Adonis <adonisw0rick@gmail.com>
2018-08-31 12:55:14 +0100

    Nuevo Modulo3

--- my_module/__manifest__.py
+++ my_module/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Adonis_module",
+    'name': "Adonis_moduleGitHub",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
