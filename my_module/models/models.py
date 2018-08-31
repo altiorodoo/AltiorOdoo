173421a3bc644f4bef6a93174a552283cd6f9994
Adonis <adonisw0rick@gmail.com>
2018-08-31 16:13:59 +0100

    Nuevo Modulo8

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -3,15 +3,13 @@
 from odoo import models, fields, api
 
 class my_module(models.Model):
+    _name = 'my_module.my_module'
 
-    def __init__(self):
-        _name = 'my_module.my_module'
-
-        name = fields.Char()
-        value = fields.Integer()
-        value2 = fields.Float(compute="_value_pc", store=True)
-        value3 = fields.Float(compute="_value_pc", store=True)
-        description = fields.Text()
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    value3 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 
     @api.depends('value')
     def _value_pc(self):
