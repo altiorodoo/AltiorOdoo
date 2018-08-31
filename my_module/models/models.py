5c57c6b81425ccce6202d44b9b330336ec1411d7
Adonis <adonisw0rick@gmail.com>
2018-08-31 17:06:16 +0100

    Nuevo Modulo9

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -9,9 +9,12 @@ class my_module(models.Model):
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     value3 = fields.Float(compute="_value_pc", store=True)
+    value4 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
 
     @api.depends('value')
     def _value_pc(self):
         self.value2 = float(self.value) / 100
         self.value3 = float(self.value)*2
+        self.value4 = float(self.value)*3
+        
